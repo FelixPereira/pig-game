bb427f496baafdaa9e961a0d557a060476aa1137
FelixPereira <felixpereira538@gmail.com>
2022-01-20 20:09:11 +0100

    Swith active player and change background

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,8 @@ const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 const current0El = document.getElementById('current--0');
 const current1El = document.getElementById('current--1');
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 
 const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
@@ -16,7 +18,7 @@ const player = document.querySelectorAll('.player');
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
-
+let activePlayer = 0;
 let currentScore = 0;
 
 // Rolling dice functionality
@@ -25,19 +27,20 @@ btnRoll.addEventListener('click', function() {
   const dice = Math.trunc(Math.random() * 6) + 1;
   
   // 2. Display dice
-  diceEl.src = `dice-${dice}.png`;
+  diceEl.src = `assets/images/dice-${dice}.png`;
   diceEl.classList.remove('hidden');
   
   // 3. Check for roll 1: if is true, switch to next player
   if(dice !== 1) {
     // Add roll to current score
     currentScore += dice;
-    current0El.textContent = currentScore;
+    document.getElementById(`current--${activePlayer}`).textContent = currentScore;
   } else {
       // Switch player
-      for(let i = 0; i < player.length; i++){
-        player[i].classList.toggle('player--active');
-        
-      }
+      currentScore = 0;
+      document.getElementById(`current--${activePlayer}`).textContent = currentScore;
+      activePlayer = activePlayer === 0 ? 1 : 0;
+      player0El.classList.toggle('player--active');
+      player1El.classList.toggle('player--active');
   }
 });
